50b233b092368cdfbdc19be4db4a6fa310c8296a
RRIMEL <144893635+RRIMEL@users.noreply.github.com>
2023-09-13 14:53:54 +0600

    Add files via upload

---
+++ Untitled1.12.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+int cricket_price =200;
+if(cricket_price <200 )
+{
+ printf("i will go\n");
+}else
+{
+    printf("i will not go\n");
+}
+
+
+
+
+
+
+
+    return 0;
+}
